9f835a17a32c65d0452127e6614bcd5d1a3569bd
Abcapon <abcapon89@gmail.com>
2023-07-12 20:14:18 +0200

    aggiunta animazione lente d'ingrandimento

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function toggleSearch() {
+    const searchResults = document.getElementById("search-input")
+    searchResults.classList.toggle("showMyBar1")
+    const button1 = document.getElementById("myButton")
+    button1.classList.toggle("move1")
+  }
+
+  
+  function toggleSearch2() {
+    const searchResults2 = document.getElementById("search-input2")
+    const button2 = document.getElementById("myButton2")
+    button2.classList.toggle("move2")
+    searchResults2.classList.toggle("showMyBar2")
+  }

549a96a100fa62b364d228f2126838721eca1bf8
Abcapon <abcapon89@gmail.com>
2023-07-14 16:17:39 +0200

    sistemato footer

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+/* funzione di animazione bottone mobile */
 function toggleSearch() {
     const searchResults = document.getElementById("search-input")
     searchResults.classList.toggle("showMyBar1")
@@ -6,7 +7,8 @@ function toggleSearch() {
     button1.classList.toggle("move1")
   }
 
-  
+  /* funzione di animazione bottone schermo grande */
+
   function toggleSearch2() {
     const searchResults2 = document.getElementById("search-input2")
     const button2 = document.getElementById("myButton2")
